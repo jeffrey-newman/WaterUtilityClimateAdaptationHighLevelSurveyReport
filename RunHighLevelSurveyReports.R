10710c05e59fcf28b89dc553283ff57a417c8531
jeffrey-newman <jeffrey.newman.au@gmail.com>
2018-03-27 11:42:24 +1030

    No longer a script per utility but one Rmarkdown file using a varaible to hold the  utility name

--- RunHighLevelSurveyReports.R
+++ RunHighLevelSurveyReports.R
@@ -1,6 +1,23 @@
 library(rmarkdown)
-render("MelbourneWaterHigherLevel.Rmd", output_format = 'all')
-render("BarwonWaterHigherLevel.Rmd", output_format = 'all')
-render("SouthEastWaterHigherLevel.Rmd", output_format = 'all')
-render("ColibanWaterHigherLevel.Rmd", output_format = 'all')
-render("CityWestWaterHigherLevel.Rmd", output_format = 'all')
+survey_data_xl_path <- "/home/a1091793/Dropbox (WaterSystems)/Projects/Climate Adaptation/Phase 1 - Decision Survey/Results/Released Survey/HighLevelResults-27Mar2018/High level survey of climate adaptation v2 aggregated.xlsx"
+out_dir <- "/home/a1091793/Dropbox (WaterSystems)/Projects/Climate Adaptation/Phase 1 - Decision Survey/Results/Released Survey/HighLevelSurveyReports"
+utility_name <- 'Water Corporation'
+render("ComparisonAnalysisTemplate.Rmd", output_format = 'pdf_document',  output_dir=out_dir, output_file=paste(utility_name, ".pdf"))
+render("ComparisonAnalysisTemplate.Rmd", output_format = 'html_document',  output_dir=out_dir, output_file=paste(utility_name, ".html"))
+utility_name <- 'Barwon Water'
+render("ComparisonAnalysisTemplate.Rmd", output_format = 'pdf_document',  output_dir=out_dir, output_file=paste(utility_name, ".pdf"))
+render("ComparisonAnalysisTemplate.Rmd", output_format = 'html_document',  output_dir=out_dir, output_file=paste(utility_name, ".html"))
+utility_name <- 'City West Water'
+render("ComparisonAnalysisTemplate.Rmd", output_format = 'pdf_document',  output_dir=out_dir, output_file=paste(utility_name, ".pdf"))
+render("ComparisonAnalysisTemplate.Rmd", output_format = 'html_document',  output_dir=out_dir, output_file=paste(utility_name, ".html"))
+utility_name <- 'Coliban Water'
+render("ComparisonAnalysisTemplate.Rmd", output_format = 'pdf_document',  output_dir=out_dir, output_file=paste(utility_name, ".pdf"))
+render("ComparisonAnalysisTemplate.Rmd", output_format = 'html_document',  output_dir=out_dir, output_file=paste(utility_name, ".html"))
+utility_name <- 'Melbourne Water'
+render("ComparisonAnalysisTemplate.Rmd", output_format = 'pdf_document',  output_dir=out_dir, output_file=paste(utility_name, ".pdf"))
+render("ComparisonAnalysisTemplate.Rmd", output_format = 'html_document',  output_dir=out_dir, output_file=paste(utility_name, ".html"))
+utility_name <- 'South East Water'
+render("ComparisonAnalysisTemplate.Rmd", output_format = 'pdf_document',  output_dir=out_dir, output_file=paste(utility_name, ".pdf"))
+render("ComparisonAnalysisTemplate.Rmd", output_format = 'html_document',  output_dir=out_dir, output_file=paste(utility_name, ".html"))
+
+
